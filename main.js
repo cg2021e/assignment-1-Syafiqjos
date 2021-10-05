5991193bf7297b94acdcb9e4e118cde653144bf7
Syafiqjos <syafiqjos@gmail.com>
2021-10-05 07:29:40 +0700

    Add Render and Position into WebGLWorld class

--- main.js
+++ main.js
@@ -9,7 +9,10 @@ function main(){
         attribute vec3 aColor;
         varying vec3 vColor;
 
+        uniform vec3 uPosition;
+
         void main(){
+            gl_PointSize = 10.0;
             gl_Position = vec4(aPosition, 1.0);
             vColor = aColor;
         }
@@ -44,24 +47,35 @@ function main(){
         0, 1, 2
     ];
 
-    world = WebGLWorld(gl)
-    world.AddObject(eraserObj)
-    world.AddObject(otherObj)
+    world = WebGLWorld(gl);
+    world.AddObject(eraserObj);
+    world.AddObject(otherObj);
     
-    world.Deploy()
+    world.Deploy();
+    world.Render();
 }
 
-main()
+main();
 
 // Objects Class
 
 function WebGLObject(gl, vertexShaderSource, fragmentShaderSource) {
     return {
         gl: gl,
+        pos: {
+            x: 0,
+            y: 0,
+            z: 0
+        },
         vertices: [],
         indices: [],
         vertexShaderSource: vertexShaderSource,
-        fragmentShaderSource: fragmentShaderSource
+        fragmentShaderSource: fragmentShaderSource,
+        buffers: {
+            vertexBuffer: null,
+            fragmentBuffer: null
+        },
+        shaderProgram: null
     }
 }
 
@@ -69,14 +83,16 @@ function WebGLWorld(gl){
     return {
         gl : gl,
         objects: [],
+        buffers: [],
+        options: {
+            isDeployed: false,
+            isRendering: false,
+        },
         AddObject(webGLObject){
             this.objects.push(webGLObject);
         },
         Deploy(){
             console.log("DEPLOY")
-
-            this.gl.clearColor(0.5, 0.5, 0.5, 0.7);
-            this.gl.clear(gl.COLOR_BUFFER_BIT);
 
             this.objects.forEach((item)=>{
                 let shaderProgram = this.gl.createProgram();
@@ -99,21 +115,71 @@ function WebGLWorld(gl){
                 this.gl.compileShader(fragmentShader);
                 this.gl.attachShader(shaderProgram, fragmentShader);
 
+                // Use Program
                 this.gl.linkProgram(shaderProgram);
                 this.gl.useProgram(shaderProgram);
 
+                // Register variable to item object
+                item.buffers = { vertexBuffer, fragmentBuffer };
+                item.shaderProgram = shaderProgram;
+
+                // Position Attribute
                 let aPosition = this.gl.getAttribLocation(shaderProgram, "aPosition");
                 this.gl.vertexAttribPointer(aPosition, 3, gl.FLOAT, false, 6 * Float32Array.BYTES_PER_ELEMENT, 0);
                 this.gl.enableVertexAttribArray(aPosition);
 
+                // Color Attribute
                 let aColor = this.gl.getAttribLocation(shaderProgram, "aColor");
                 this.gl.vertexAttribPointer(aColor, 3, gl.FLOAT, false, 6 * Float32Array.BYTES_PER_ELEMENT, 3 * Float32Array.BYTES_PER_ELEMENT);
                 this.gl.enableVertexAttribArray(aColor);
                 
+                // Position Uniform
+                let uPosition = this.gl.getUniformLocation(shaderProgram, "uPosition");
+                this.gl.uniform3fv(uPosition, [item.pos.x, item.pos.y, item.pos.z]);
+
                 this.gl.drawElements(this.gl.TRIANGLES, item.indices.length, this.gl.UNSIGNED_SHORT, 0);
 
                 this.gl.bindBuffer(this.gl.ARRAY_BUFFER, null);
                 this.gl.bindBuffer(this.gl.ELEMENT_ARRAY_BUFFER, null);
+            });
+
+            isDeployed = true;
+            isRendering = true;
+        },
+        Render(){
+            if (isDeployed) {
+                if (isRendering){
+                    this.clearBackgroundRender();
+                    this.objectsRender();
+                }
+                requestAnimationFrame(() => { this.Render() });
+            } else {
+                console.log('World not deployed yet.');
+            }
+        },
+        clearBackgroundRender(){
+            this.gl.clearColor(0.5, 0.5, 0.5, 0.7);
+            this.gl.clear(gl.COLOR_BUFFER_BIT);
+        },
+        objectsRender(){
+            this.objects.forEach((item)=>{
+                vertexBuffer = item.buffers.vertexBuffer;
+                fragmentBuffer = item.buffers.fragmentBuffer;
+                shaderProgram = item.shaderProgram;
+
+                if (vertexBuffer) this.gl.bindBuffer(this.gl.ARRAY_BUFFER, vertexBuffer);
+                if (fragmentBuffer) this.gl.bindBuffer(this.gl.ELEMENT_ARRAY_BUFFER, fragmentBuffer);
+
+                if (shaderProgram) {
+                    // Position Uniform
+                    let uPosition = this.gl.getUniformLocation(shaderProgram, "uPosition");
+                    this.gl.uniform3fv(uPosition, [item.pos.x, item.pos.y, item.pos.z]);
+                }
+
+                this.gl.drawElements(this.gl.TRIANGLES, item.indices.length, this.gl.UNSIGNED_SHORT, 0);
+
+                if (vertexBuffer) this.gl.bindBuffer(this.gl.ARRAY_BUFFER, null);
+                if (fragmentBuffer) this.gl.bindBuffer(this.gl.ELEMENT_ARRAY_BUFFER, null);
             });
         }
     }
